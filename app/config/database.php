2e448b0b41d3beacb432f310b121fb424055cd5f
JuanProgra <ljprogra911@gmail.com>
2015-07-12 20:20:15 -0600

    Migracion cambios

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'CineKinal',
+			'username'  => 'root',
+			'password'  => 'Juan5708',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
